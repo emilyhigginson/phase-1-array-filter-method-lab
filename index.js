30f0d6bc91b4ec839a274e2c5158e82352ffb65f
Emily <emilyhigginson14@gmail.com>
2022-01-12 10:41:05 -0500

    commit tree 4d805bd9dab2dd9f46bcd682f22a63f87c71f7fb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+
+
+function findMatching(drivers, match) {
+    return drivers.filter(
+        (driverMatch) => driverMatch.toLowerCase() === match.toLowerCase()
+    );
+}
+function fuzzyMatch(drivers, beginningMatch) {
+    return drivers.filter(
+        (driverMatch) => driverMatch.toLowerCase().indexOf(beginningMatch.toLowerCase()) === 0
+    );
+}
+function matchName(drivers, driverName){
+    return drivers.filter((data) => data.name === driverName);
+}
